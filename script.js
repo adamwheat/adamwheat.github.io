bdcfffb41bc6e1184147f45ce570265d185ae771
adamwheat <awheattu@gmail.com>
2016-10-30 16:53:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+var mn = $(".main-nav");
+var mns = "main-nav-scrolled";
+var hdr = $('header').height();
+
+$(window).scroll(function() {
+ if( $(this).scrollTop() > hdr ) {
+  mn.addClass(mns);
+ } else {
+  mn.removeClass(mns);
+ }
+});
+alert("working");
+});
