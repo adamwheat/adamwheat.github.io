6026d7110c2cfa16d069998b20e6db8dbd56123d
adamwheat <awheattu@gmail.com>
2016-10-30 19:24:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var mns = "main-nav-scrolled";
 var hdr = $('header').height();
 
 $(window).scroll(function() {
- if( $(this).scrollTop() > 400 ) {
+ if( $(this).scrollTop() > 450 ) {
   mn.addClass(mns);
  } else {
   mn.removeClass(mns);
