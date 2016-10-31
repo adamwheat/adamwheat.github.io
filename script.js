24189852d226da43664fead41edf1e7465994f8c
adamwheat <awheattu@gmail.com>
2016-10-30 22:57:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var mns = "main-nav-scrolled";
 var hdr = $('header').height();
 
 $(window).scroll(function() {
- if( $(this).scrollTop() > 250 ) {
+ if( $(this).scrollTop() > 350 ) {
   mn.addClass(mns);
  } else {
   mn.removeClass(mns);
